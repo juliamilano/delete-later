5af2619dfed988eb19760887484fa95cb7f6c14c
juliamilano <t.yulya97@gmail.com>
2020-11-25 10:59:52 +0200

    func delete

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import {getTracks} from './action/tracks';
 
+
+
 class App extends Component {
     addTrack(){
         //console.log("addTrack", this.trackInput.value, this.props.onAddTrack);
@@ -12,8 +14,22 @@ class App extends Component {
         console.log("searchInput", this.searchInput.value);
         this.props.onFindTrack(this.searchInput.value);
     }
+    editData(id){
+        console.log("editData", id);
+        this.props.onEditData(id);
+    }
+    deleteData(id){
+        console.log("deleteData", id);
+        this.props.onDeleteData(id);
+    }
+    btnEdit = "<button onClick = \{this.editData.bind\(this, track.id\)\}> ok </button>";
+    flag = false;
+    editInput(){
+        this.flag = true;
+    }
     render(){
        // console.log(this.props.testStore);
+        let space = {display:'inline-block', background: "pink",  width: '20px', height:'3px'};
         return (
             <div>
                 <div>
@@ -30,12 +46,22 @@ class App extends Component {
                 <ul>
                     {
                         this.props.tracks.map((track, index) =>
-                        { return <li key = { index } >{ track.name }</li>}
+                        {
+                            return (
+                                <li key = { index } >{ track.name }
+                            <div style = {space}></div>
+                                <button onClick = {this.editInput.bind(this)}> edit </button>
+
+                            <div style = {space}></div>
+                                <button onClick = {this.deleteData.bind(this, track.id)}> del </button>
+                            </li>)
+                        }
                     )}
                 </ul>
             </div>
         );
     }
+
 }
 
 export default connect(
@@ -54,8 +80,15 @@ export default connect(
         onFindTrack: (name) => {
             dispatch ({type: 'FIND_TRACK', payload: name});
         },
-        onGetTracks: () => {
-            dispatch(getTracks());
+        onGetTracks: (id) => {
+            dispatch(getTracks(id));
+        },
+        onEditData : (id, data) => {
+            dispatch ({type: 'EDIT_TRACK', payload: {id: id, text: data}});
+        },
+        onDeleteData : (id) => {
+            dispatch ({type: 'DELETE_TRACK', payload: id});
         }
+
     })
 )(App);
--- src/reducers/tracks.js
+++ src/reducers/tracks.js
@@ -2,13 +2,27 @@ const initialState = [];
 
 export default function playlist(state = initialState, action) {
     // console.log("action ", action, " state = " , state); //1 , 3
-    if (action.type === 'ADD_TRACK'){
+    if (action.type === 'ADD_TRACK') {
+        return [
+            ...state,
+            action.payload
+        ];
+    } else if (action.type === 'DELETE_TRACK') {
+        let data = [...state];
+        const filtered = data.filter((item) => item.id !== action.payload);
+        return [
+            ...filtered
+        ];
+    }
+    else if (action.type === 'EDIT_TRACK') {
+        //console.log("EDIT_TRACK action ", action, " state = " , state); //1 , 3
         return [
             ...state,
             action.payload
         ];
     } else if (action.type === 'FETCH_TRACKS_SUCCESS'){
-        return action.payload;
+        debugger;
+        return [...state, ...action.payload];
     }
     return state;
 }
